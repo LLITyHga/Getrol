7835bd3aaa0b63806d1ff361f553fa8196311983
Andrii Pikus <llityhga@gmail.com>
2025-01-03 16:35:29 +0200

    start homeview

---
+++ Getrol/Presentation/HomeFlow/View/HomeView.swift
@@ -0,0 +1,79 @@
+//
+//  HomeView.swift
+//  Getrol
+//
+//  Created by Andrii Pikus on 03.01.2025.
+//
+
+import SwiftUI
+import MapKit
+
+struct HomeView: View {
+    
+    @State private var searchText = ""
+    @State private var cameraPosition = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 50.4501, longitude: 30.5234), // Центр Києва
+            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        )
+    )
+
+    var body: some View {
+        ZStack {
+            Map(position: $cameraPosition) {
+                       // Карта без додаткових маркерів
+                   }
+                   .mapStyle(.standard()) // Стандартний стиль карти
+                   .ignoresSafeArea(edges: .all)
+            VStack(spacing: 0) {
+                searchFeld()
+                
+                routeButtons()
+                Spacer()
+            }
+            .padding(.top, 24)
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
+
+extension HomeView {
+    @ViewBuilder
+    func searchFeld () -> some View {
+        TextField("Search", text: $searchText)
+            .padding(.horizontal, 16)
+            .frame(height: 56)
+            .background(Color.bg)
+            .cornerRadius(8)
+            .padding(.horizontal, 16)
+    }
+    
+    @ViewBuilder
+    func routeButtons() -> some View {
+        HStack(spacing: 0) {
+            Spacer()
+            VStack(spacing: 0) {
+                Button(action: {
+                    // do smtng
+                }) {
+                    Image(.routeImg)
+                        .frame(width: 42, height: 42)
+                        
+                }
+                .padding(.vertical, 16)
+                .padding(.trailing, 16)
+
+                Button(action: {
+                    // do smtng
+                }) {
+                    Image(.locationImg)
+                        .frame(width: 42, height: 42) // задайте потрібний розмір
+                }
+                .padding(.trailing, 16)
+            }
+        }
+    }
+}
